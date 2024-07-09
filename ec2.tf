55c0feb41da6c603d1d1ae4359d076afe0e4c9d3
vamsibakka <vamsikrishna.123.vk@gmail.com>
2024-07-09 16:29:19 +0530

    added the aws_instance resource

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,34 @@
+# To fetch the ami id from the list of ami in aws to create the ec2 instance resource
 data "aws_ami" "machine"{
     most_recent = true
     filter{
         name = "name"
-        values = [ "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*" ]
+        values = [ "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*" ] # checks the ami name from ami's 
     }
     filter {
       name = "virtualization-type"
-      values = [ "hvm" ]
+      values = [ "hvm" ]  # checks for the virtualization type 
     }
-    owners = [ "099720109477" ]
+    owners = [ "099720109477" ] # this is the owner-id provided in the ami information it is not the "iam user id"
 }
+# it displays the ami-id which is fetched from the above data block in the terminal 
 output "machineid" {
     value = data.aws_ami.machine.id  
+}
+# creating the aws instance resource 
+resource "aws_instance" "sample" {
+    ami = data.aws_ami.machine.id # the ami_id from the data resource
+    associate_public_ip_address = true # it will enable the public-ip address
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro" # instance family type
+    subnet_id = aws_subnet.subnet_tr.id # instance will be created in the subnet you created
+    vpc_security_group_ids = [aws_security_group.sg_terraform.id] 
+    key_name = "new_key" # To login through the ssh which you created and added as keypairs in your aws account
+    tags = {
+      "Name" = "terraform" 
+    }
+    depends_on = [
+        aws_vpc.terraform_vpc,
+        aws_subnet.subnet_tr
+     ]
 }
